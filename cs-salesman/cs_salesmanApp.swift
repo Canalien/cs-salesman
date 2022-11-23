704585b34b58e224b3d650032f8fa8cb204e6b3b
Canalien <thecanalien@gmail.com>
2022-11-23 13:28:24 +0100

    Initial Commit

---
+++ cs-salesman/cs_salesmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs_salesmanApp.swift
+//  cs-salesman
+//
+//  Created by Canalien on 23.11.22.
+//
+
+import SwiftUI
+
+@main
+struct cs_salesmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
